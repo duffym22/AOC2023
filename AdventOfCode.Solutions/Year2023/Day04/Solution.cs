a87fdc6d50bf234ed9e6b72487a04d8cffe7473b
Matthew Duffy <duffym22@gmail.com>
2023-12-15 15:52:52 -0600

    WIP

--- AdventOfCode.Solutions/Year2023/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2023/Day04/Solution.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode.Solutions.Year2023.Day04;
 class Solution : SolutionBase
 {
     List<string> lines = new List<string>();
-    public Solution() : base(04, 2023, "") { }
+    public Solution() : base(04, 2023, "", true) { }
 
     protected override string SolvePartOne()
     {
@@ -21,7 +21,7 @@ class Solution : SolutionBase
     private void ParseCards(List<string> lines, out List<Card> cards)
     {
         cards = new List<Card>();
-        foreach (string line in lines) 
+        foreach (string line in lines)
         {
             //Split on | first to get left and right halves
             string[] firstSp = line.Split('|');
@@ -46,27 +46,58 @@ class Solution : SolutionBase
         List<Card> cards;
         ParseCards(lines, out cards);
 
-        Dictionary<int, int> cardsWon = new Dictionary<int, int>();
-        Dictionary<int, int> cardInstances = new Dictionary<int, int>();
-        cards.ForEach(x => cardsWon.Add(x.ID, x.CardCopiesWon));
-        cards.ForEach(x => cardInstances.Add(x.ID, 1));
+        Dictionary<int, int> ogCards = new Dictionary<int, int>();
+        cards.ForEach(x => ogCards.Add(x.ID, x.CardCopiesWon));
 
-        for (int i = 1; i < cardsWon.Keys.Count - 1; i++)
+        //Dictionary
+        //  Key: Card Number
+        //  Value: Count of card copies (not including originals)
+        Dictionary<int, int> copyCards = new Dictionary<int, int>();
+
+        foreach (int key in ogCards.Keys)
         {
-            int incr = cardsWon[i];
-            int idx;
-            //Count as many times up as the value unless it goes out of bounds
-            for (int j = 0; j < incr; j++)
+            //Initialize the dictionary with a count of 0 
+            copyCards.Add(key, 0);
+        }
+
+        /// Order of Operations
+        ///     1. Process current card and increase/add card copy counts
+        ///     2. Process card copies
+        ///     3. Go to next card
+        for (int i = ogCards.Keys.First(); i <= ogCards.Keys.Count; i++)
+        {
+            // 1. Process current card
+            ProcessCard(i, ref ogCards, ref copyCards);
+
+            // 2. Process EACH card copies (if any)
+            for (int j = 0; j < copyCards[i]; j++)
             {
-                idx = i + 1 + j;
-                if(idx <= cardsWon.Keys.Count)
-                    cardInstances[idx]++;
+                ProcessCard(i, ref ogCards, ref copyCards);
             }
         }
 
-        //This is not it - it doesn't cascade to account for newly added cards during processing
-        return cardInstances.Values.Sum().ToString();
+        //Attempt 1: 2410 (too low)
+        return (ogCards.Sum(x => x.Value) + copyCards.Sum(x => x.Value)).ToString();
     }
+
+    //Returns 
+    private void ProcessCard(int currentCardID, ref Dictionary<int, int> cardsWon, ref Dictionary<int, int> cardCopies)
+    {
+        int copiesWon = cardsWon[currentCardID];
+        int range = currentCardID + copiesWon;
+
+        // 1.1 Add card copy counts
+        // Range check to make sure we dont go past the total number of cards
+        // Current Position + Card Copes <= Max cards = Safe to process cards up to the number of card copies won
+        // Current Position + Card Copes > Max cards = Stop processing cards at max card count
+        int limit = range <= cardsWon.Keys.Count ? range : cardsWon.Keys.Count;
+
+        for (int j = currentCardID + 1; j <= limit; j++)
+        {
+            cardCopies[j]++;
+        }
+    }
+
 }
 
 class Card
@@ -84,3 +115,17 @@ class Card
         GameNumbers = game;
     }
 }
+
+
+//for (int i = 1; i < cardsWon.Keys.Count - 1; i++)
+//{
+//    int incr = cardsWon[i];
+//    int idx;
+//    //Count as many times up as the value unless it goes out of bounds
+//    for (int j = 0; j < incr; j++)
+//    {
+//        idx = i + 1 + j;
+//        if (idx <= cardsWon.Keys.Count)
+//            cardInstances[idx]++;
+//    }
+//}
